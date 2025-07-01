bcdf6171c76dc6845f3a4d85eadf4ee6b37f2f54
ddr525 <58463081+ddr525@users.noreply.github.com>
2025-07-01 17:03:02 +0500

    Add files via upload

--- _HeatBalance.py
+++ _HeatBalance.py
@@ -6,6 +6,7 @@ class HeatBalance(ctk.CTkFrame):
         
         self.font=ctk.CTkFont(size=13, weight="bold")
         self.view_data = "MВт"
+        self.cells = {} 
         
     def update(self, heating_data):
         table = ctk.CTkFrame(self, fg_color="black", border_color="black", border_width=2)
@@ -92,75 +93,78 @@ class HeatBalance(ctk.CTkFrame):
         self.newcell(table, 12, 3, "", value="Методическая зона низ|Расход тепла|Потери через СИО")
         self.newcell(table, 13, 3, "", value="Методическая зона низ|Расход тепла|Потери через окно посада")
         self.newcell(table, 14, 3, "", value="Методическая зона низ|Расход тепла|Итого расход")
-        self.newcell(table, 15, 3, "")
+        self.newcell(table, 15, 3, "-")
+# ["в первой сварочной зоне", "в зоне нижнего подогрева 2", "во второй сварочной зоне", "в зоне нижнего подогрева 4", "в томильной зоне"]
+        self.newcell(table, 3, 4, "", value="Первая сварочная зона|Приход тепла|Химическое тепло топлива")
+        self.newcell(table, 4, 4, "", value="Первая сварочная зона|Приход тепла|Физическое тепло воздуха")
+        self.newcell(table, 5, 4, "", value="Первая сварочная зона|Приход тепла|Физическое тепло газа")
+        self.newcell(table, 6, 4, "", value="Первая сварочная зона|Приход тепла|Тепло транзитных газов")
+        self.newcell(table, 7, 4, "", value="Первая сварочная зона|Приход тепла|Итого приход")
 
-        self.newcell(table, 3, 4, "")
-        self.newcell(table, 4, 4, "")
-        self.newcell(table, 5, 4, "")
-        self.newcell(table, 6, 4, "")
-        self.newcell(table, 7, 4, "")
+        self.newcell(table, 9, 4, "", value="Первая сварочная зона|Расход тепла|Тепло на нагрев металла")
+        self.newcell(table, 10, 4, "", value="Первая сварочная зона|Расход тепла|Потери через кладку")
+        self.newcell(table, 11, 4, "", value="Первая сварочная зона|Расход тепла|Тепло уходящее с дымом")
+        self.newcell(table, 12, 4, "", value="Первая сварочная зона|Расход тепла|Потери через СИО")
+        self.newcell(table, 13, 4, "-")
+        self.newcell(table, 14, 4, "", value="Первая сварочная зона|Расход тепла|Итого расход")
+        self.newcell(table, 15, 4, "", value="Первая сварочная зона|Расход топлива, тыс. м³/час")
 
-        self.newcell(table, 9, 4, "")
-        self.newcell(table, 10, 4, "")
-        self.newcell(table, 11, 4, "")
-        self.newcell(table, 12, 4, "")
-        self.newcell(table, 13, 4, "")
-        self.newcell(table, 14, 4, "")
-        self.newcell(table, 15, 4, "")
+        self.newcell(table, 3, 5, "", value="Зона нижнего подогрева 2|Приход тепла|Химическое тепло топлива")
+        self.newcell(table, 4, 5, "", value="Зона нижнего подогрева 2|Приход тепла|Физическое тепло воздуха")
+        self.newcell(table, 5, 5, "", value="Зона нижнего подогрева 2|Приход тепла|Физическое тепло газа")
+        self.newcell(table, 6, 5, "", value="Зона нижнего подогрева 2|Приход тепла|Тепло транзитных газов")
+        self.newcell(table, 7, 5, "", value="Зона нижнего подогрева 2|Приход тепла|Итого приход")
 
-        self.newcell(table, 3, 5, "")
-        self.newcell(table, 4, 5, "")
-        self.newcell(table, 5, 5, "")
-        self.newcell(table, 6, 5, "")
-        self.newcell(table, 7, 5, "")
+        self.newcell(table, 9, 5, "", value="Зона нижнего подогрева 2|Расход тепла|Тепло на нагрев металла")
+        self.newcell(table, 10, 5, "", value="Зона нижнего подогрева 2|Расход тепла|Потери через кладку")
+        self.newcell(table, 11, 5, "", value="Зона нижнего подогрева 2|Расход тепла|Тепло уходящее с дымом")
+        self.newcell(table, 12, 5, "", value="Зона нижнего подогрева 2|Расход тепла|Потери через СИО")
+        self.newcell(table, 13, 5, "-")
+        self.newcell(table, 14, 5, "", value="Зона нижнего подогрева 2|Расход тепла|Итого расход")
+        self.newcell(table, 15, 5, "", value="Зона нижнего подогрева 2|Расход топлива, тыс. м³/час")
 
-        self.newcell(table, 9, 5, "")
-        self.newcell(table, 10, 5, "")
-        self.newcell(table, 11, 5, "")
-        self.newcell(table, 12, 5, "")
-        self.newcell(table, 13, 5, "")
-        self.newcell(table, 14, 5, "")
-        self.newcell(table, 15, 5, "")
+        
+        self.newcell(table, 3, 6, "", value="Вторая сварочная зона|Приход тепла|Химическое тепло топлива")
+        self.newcell(table, 4, 6, "", value="Вторая сварочная зона|Приход тепла|Физическое тепло воздуха")
+        self.newcell(table, 5, 6, "", value="Вторая сварочная зона|Приход тепла|Физическое тепло газа")
+        self.newcell(table, 6, 6, "", value="Вторая сварочная зона|Приход тепла|Тепло транзитных газов")
+        self.newcell(table, 7, 6, "", value="Вторая сварочная зона|Приход тепла|Итого приход")
 
-        self.newcell(table, 3, 6, "")
-        self.newcell(table, 4, 6, "")
-        self.newcell(table, 5, 6, "")
-        self.newcell(table, 6, 6, "")
-        self.newcell(table, 7, 6, "")
+        self.newcell(table, 9, 6, "", value="Вторая сварочная зона|Расход тепла|Тепло на нагрев металла")
+        self.newcell(table, 10, 6, "", value="Вторая сварочная зона|Расход тепла|Потери через кладку")
+        self.newcell(table, 11, 6, "", value="Вторая сварочная зона|Расход тепла|Тепло уходящее с дымом")
+        self.newcell(table, 12, 6, "-")
+        self.newcell(table, 13, 6, "-")
+        self.newcell(table, 14, 6, "", value="Вторая сварочная зона|Расход тепла|Итого расход")
+        self.newcell(table, 15, 6, "", value="Вторая сварочная зона|Расход топлива, тыс. м³/час")
 
-        self.newcell(table, 9, 6, "")
-        self.newcell(table, 10, 6, "")
-        self.newcell(table, 11, 6, "")
-        self.newcell(table, 12, 6, "")
-        self.newcell(table, 13, 6, "")
-        self.newcell(table, 14, 6, "")
-        self.newcell(table, 15, 6, "")
+        self.newcell(table, 3, 7, "", value="Зона нижнего подогрева 4|Приход тепла|Химическое тепло топлива")
+        self.newcell(table, 4, 7, "", value="Зона нижнего подогрева 4|Приход тепла|Физическое тепло воздуха")
+        self.newcell(table, 5, 7, "", value="Зона нижнего подогрева 4|Приход тепла|Физическое тепло газа")
+        self.newcell(table, 6, 7, "-")
+        self.newcell(table, 7, 7, "", value="Зона нижнего подогрева 4|Приход тепла|Итого приход")
 
-        self.newcell(table, 3, 7, "")
-        self.newcell(table, 4, 7, "")
-        self.newcell(table, 5, 7, "")
-        self.newcell(table, 6, 7, "")
-        self.newcell(table, 7, 7, "")
-        self.newcell(table, 9, 7, "")
-        self.newcell(table, 10, 7, "")
-        self.newcell(table, 11, 7, "")
-        self.newcell(table, 12, 7, "")
-        self.newcell(table, 13, 7, "")
-        self.newcell(table, 14, 7, "")
-        self.newcell(table, 15, 7, "")
+        self.newcell(table, 9, 7, "", value="Зона нижнего подогрева 4|Расход тепла|Тепло на нагрев металла")
+        self.newcell(table, 10, 7, "", value="Зона нижнего подогрева 4|Расход тепла|Потери через кладку")
+        self.newcell(table, 11, 7, "", value="Зона нижнего подогрева 4|Расход тепла|Тепло уходящее с дымом")
+        self.newcell(table, 12, 7, "", value="Зона нижнего подогрева 4|Расход тепла|Потери через СИО")
+        self.newcell(table, 13, 7, "-")
+        self.newcell(table, 14, 7, "", value="Зона нижнего подогрева 4|Расход тепла|Итого расход")
+        self.newcell(table, 15, 7, "", value="Зона нижнего подогрева 4|Расход топлива, тыс. м³/час")
 
-        self.newcell(table, 3, 8, "")
-        self.newcell(table, 4, 8, "")
-        self.newcell(table, 5, 8, "")
-        self.newcell(table, 6, 8, "")
-        self.newcell(table, 7, 8, "")
-        self.newcell(table, 9, 8, "")
-        self.newcell(table, 10,8, "")
-        self.newcell(table, 11, 8, "")
-        self.newcell(table, 12, 8, "")
-        self.newcell(table, 13, 8, "")
-        self.newcell(table, 14, 8, "")
-        self.newcell(table, 15, 8, "")
+        self.newcell(table, 3, 8, "", value="Томильная зона|Приход тепла|Химическое тепло топлива")
+        self.newcell(table, 4, 8, "", value="Томильная зона|Приход тепла|Физическое тепло воздуха")
+        self.newcell(table, 5, 8, "", value="Томильная зона|Приход тепла|Физическое тепло газа")
+        self.newcell(table, 6, 8, "-")
+        self.newcell(table, 7, 8, "", value="Томильная зона|Приход тепла|Итого приход")
+
+        self.newcell(table, 9, 8, "", value="Томильная зона|Расход тепла|Тепло на нагрев металла")
+        self.newcell(table, 10, 8, "", value="Томильная зона|Расход тепла|Потери через кладку")
+        self.newcell(table, 11, 8, "", value="Томильная зона|Расход тепла|Тепло уходящее с дымом")
+        self.newcell(table, 12, 8, "-")
+        self.newcell(table, 13, 8, "-")
+        self.newcell(table, 14, 8, "", value="Томильная зона|Расход тепла|Итого расход")
+        self.newcell(table, 15, 8, "", value="Томильная зона|Расход топлива, тыс. м³/час")
 
     def newcell(self, table, row, column, text, value = "", columnspan=0, rowspan=0, sticky="ew", wraplength=0, border_color="black", border_width=0, fg_color="#7c7878"):
         block=ctk.CTkFrame(table, border_color=border_color, border_width=border_width, fg_color=fg_color, corner_radius=0)
@@ -174,23 +178,43 @@ class HeatBalance(ctk.CTkFrame):
         block.grid_rowconfigure(0, weight=1)
         block.grid_columnconfigure(0, weight=1)
         label_text = text
+        path = None
         if len(value) > 0:
             path = value.split("|")
+            zone = self.result.get(path[0], {})
+            section = zone.get(path[1], {})
             if "," in value:
                 label_text = self.result[path[0]][path[1]]
-            else:    
-                label_text = self.result[path[0]][path[1]][f"{path[2]}, {self.view_data}"]
+            else:
+            # label_text = "нет данных"
+                key = f"{path[2]}, {self.view_data}"
+                if key in section:
+                    label_text = section[key]
+        else:
+            path = None
             
         cell = ctk.CTkLabel(block, 
                             text=label_text,
                             fg_color="transparent", corner_radius=0, wraplength=wraplength, font=self.font)
         cell.grid(row=0, column=0, padx=15, pady=1, sticky="ew")
+        if path:
+            self.cells["|".join(path)] = cell  # сохраняем метку
+        # if len(value) > 0:
+        #     self.cells["|".join(path)] = cell
+
         if(columnspan > 0):
             block.grid_configure(columnspan=columnspan)
         if(rowspan > 0):
             block.grid_configure(rowspan=rowspan)
 
+
     def change(self):
         self.view_data = "%" if self.view_data == "MВт" else "MВт"
         print(self.view_data)
-        # self.update()
+        for key, label in self.cells.items():
+            path = key.split("|")
+            try:
+                value = self.result[path[0]][path[1]].get(f"{path[2]}, {self.view_data}", "-")
+                label.configure(text=value)
+            except Exception as e:
+                label.configure(text="-")
